b87eb20907ce761bd6bdb0a73b43733c9eed0b6d
jesusdrp09 <jesusdrp09@gmail.com>
2022-07-29 22:08:30 -0500

    Cambio de orden abreviaciones y contracciones

--- Eliminar nombres.py
+++ Eliminar nombres.py
@@ -8,13 +8,13 @@ contracciones = pandas.read_csv("contracciones.csv", delimiter=";")
 print(abreviaciones)
 tweets = pandas.DataFrame(data=dataframe)
 
-#Reemplazando abreviaciones
-for i in range(len(abreviaciones['abreviacion'])):
-    tweets['col1'].replace(to_replace=abreviaciones['abreviacion'][i], value = abreviaciones['palabra'][i], regex=True, inplace=True)
-
 #Reemplazando contracciones
 for i in range(len(contracciones['contraccion'])):
     tweets['col1'].replace(to_replace=contracciones['contraccion'][i], value = contracciones['palabra'][i], regex=True, inplace=True)
+    
+#Reemplazando abreviaciones
+for i in range(len(abreviaciones['abreviacion'])):
+    tweets['col1'].replace(to_replace=abreviaciones['abreviacion'][i], value = abreviaciones['palabra'][i], regex=True, inplace=True)
 
 #Reemplazando nombres de usuario    
 tweets['col1'].replace(to_replace=r'@[\w]{1,15}', value="username", regex=True, inplace=True)
