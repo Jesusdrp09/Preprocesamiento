404e1a1535bd19bad88158c2192e0960721b554f
jesusdrp09 <jesusdrp09@gmail.com>
2022-08-21 22:55:38 -0500

    Solucion a problema de final - Domingo

--- ejemplo.py
+++ ejemplo.py
@@ -1,17 +1,19 @@
-import twint 
-import os
-import prepro
 import time
-
 initial_time = time.time()
+
+import twint
+import os 
+import prepro
+
 os.remove("datos.csv")
-file = open("datos.csv", "w")
+file = open("datos.csv", "w") 
 
 config = twint.Config()
-config.Search = "feliz OR triste OR covid OR coronavirus"
+config.Limit = 10
+config.Search = "al"
 config.Lang = "es"
 config.Since = "2020-03-14"
-config.Near = "Colombia" 
+config.Near = "Colombia"
 config.Filter_retweets = True
 config.Store_csv = True
 config.Output = "./datos.csv"
--- prepro.py
+++ prepro.py
@@ -1,18 +1,20 @@
+from sys import flags
 import pandas
+
 def preprocesamiento():
     #dataframe
-    dataframe = pandas.read_csv("datos.csv", delimiter=",")
+    dataframe = pandas.read_csv("datos.csv", delimiter=",", header=None)
     abreviaciones = pandas.read_csv("abreviaciones.csv", delimiter=";")
     contracciones = pandas.read_csv("contracciones.csv", delimiter=";")
     tweets = pandas.DataFrame(data=dataframe)
 
     #Reemplazando contracciones
     for i in range(len(contracciones['contraccion'])):
-        tweets.iloc[:,10].replace(to_replace=contracciones['contraccion'][i], value = contracciones['palabra'][i], regex=True, inplace=True)
+        tweets.iloc[:,10].replace(to_replace=r'\s'+contracciones['contraccion'][i]+'\s', value =" "+contracciones['palabra'][i]+" ", inplace=True, regex=True)
         
     #Reemplazando abreviaciones
     for i in range(len(abreviaciones['abreviacion'])):
-        tweets.iloc[:,10].replace(to_replace=abreviaciones['abreviacion'][i], value = abreviaciones['palabra'][i], regex=True, inplace=True)
+        tweets.iloc[:,10].replace(to_replace=r""+abreviaciones['abreviacion'][i], value = abreviaciones['palabra'][i], regex=True, inplace=True)
 
     #Reemplazando nombres de usuario    
     tweets.iloc[:,10].replace(to_replace=r'@[\w]{1,15}', value="username", regex=True, inplace=True)
@@ -20,5 +22,4 @@ def preprocesamiento():
     #Guardando los cambios los cambios
     tweets.to_csv("preprocesamiento.csv")
     
-
 
